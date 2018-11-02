0b6e4326d98950d48fb2457f45fcfb6bacc76365
Joseph Tomasso <jatboygriefs@gmail.com>
2018-11-02 10:49:47 -0400

    Basic repository setup
    Working main menu and audio mixer, potentially useful assets imported

---
+++ Minesweeper3D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public AudioMixer audioMixer;
+
+	public void PlayButton() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load next scene.
+    }
+
+    public void QuitButton() {
+        Application.Quit();
+    }
+
+    public void AdjustMasterVolume(float volume) {
+        if(!Application.isEditor)
+            audioMixer.SetFloat("MasterVolume", volume);   
+    }
+
+    public void AdjustMusicVolume(float volume) {
+        if (!Application.isEditor)
+            audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void AdjustSoundVolume(float volume) {
+        if (!Application.isEditor)
+            audioMixer.SetFloat("SoundVolume", volume);
+    }
+
+}
