a4fbae35820af6efb5318301dd48f90c3c24d65e
Alexandre Gallant <caraibe888888@gmail.com>
2018-11-02 22:34:53 -0400

    Started implementing the possibility to move the camera.

---
+++ Minesweeper3D/Assets/Settings/KeyBinding.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Settings
+{
+    public static class KeyBinding
+    {
+        public static KeyCode MoveLeft = KeyCode.A;
+        public static KeyCode MoveRight = KeyCode.D;
+        public static KeyCode MoveUp = KeyCode.W;
+        public static KeyCode MoveDown = KeyCode.S;
+    }
+}
